e2d25c21a7470b83aeabc230ed0f3ee529781c08
GabrielSWilliams <31830986+GabrielSWilliams@users.noreply.github.com>
2017-09-10 17:33:48 -0400

    Made changes to lighting

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -2,24 +2,68 @@
 var loader = new THREE.JSONLoader();
 var radius = 300, theta = 0;
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
+scene.fog = new THREE.Fog( scene.background, 1, 5000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
+
+// hemilight
+
 hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-hemiLight.color.setHSL( 0.6, 1, 0.6 );
+hemiLight.color.setHSL( 0.1, 1, 1 );
 hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
 hemiLight.position.set( 0, 50, 0 );
 hemiLight.visible = true;
 scene.add( hemiLight );
+
 hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
 scene.add( hemiLightHelper );
 scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
+
+// directional light
+
+dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+dirLight.color.setHSL( 0.1, 1, 0.95 );
+dirLight.position.set( -1, 1.75, 1 );
+dirLight.position.multiplyScalar( 30 );
+scene.add( dirLight );
+
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+
+
+// skydome
+
+var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+var uniforms = {
+	topColor:    { value: new THREE.Color( 0x0077ff ) },
+	bottomColor: { value: new THREE.Color( 0x0077ff ) },
+	offset:      { value: 33 },
+	exponent:    { value: 0.6 }
+};
+uniforms.topColor.value.copy( hemiLight.color );
+
+scene.fog.color.copy( uniforms.bottomColor.value );
+
+var skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+var skyMat = new THREE.ShaderMaterial( { vertexShader: vertexShader, fragmentShader: fragmentShader, uniforms: uniforms, side: THREE.BackSide } );
+var sky = new THREE.Mesh( skyGeo, skyMat );
+scene.add( sky );
+
+
+// renderer
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// camera
 
-camera.position.z = 5;
-
+camera.position.x = 0;
+camera.position.y = 150;
+camera.position.z = 200;
+var controls = new THREE.OrbitControls( camera );
 
 
 // load a resource
@@ -32,7 +76,7 @@ loader.load(
 	function ( geometry, materials ) {
 
     var material = new THREE.MeshPhongMaterial( {
-        color: 0xff0000,
+        color: 0xD3D3D3,
         polygonOffset: true,
         polygonOffsetFactor: 1, // positive value pushes polygon further away
         polygonOffsetUnits: 1
@@ -43,12 +87,9 @@ loader.load(
 );
 
 function animate() {
-	requestAnimationFrame( animate );
-        theta += 0.1;
-				camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
-				camera.lookAt( scene.position );
 	renderer.render( scene, camera );
+
+	requestAnimationFrame( animate );
+	controls.update()
 }
 animate();
